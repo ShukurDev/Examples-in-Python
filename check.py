142780206d0722df35da5322607aabf3a88e4154
Shukurali <81503330+Shukurali777@users.noreply.github.com>
2021-07-05 07:52:50 -0700

    complete

---
+++ check.py
@@ -0,0 +1,37 @@
+import unittest
+#from m36_Task import matn
+
+# class NameTest(unittest.TestCase):
+#     def test_matn(self):
+#         matn_m = matn("Shukurali Rezamonov Sardorovich.")
+#         self.assertEqual(matn_m,"Shukurali Rezamonov Sardorovich.")
+# unittest.main()
+
+
+
+# from m36_Task import katta
+
+# class Value(unittest.TestCase):
+#     def test_value(self):
+#         value=katta(2,3,4)
+#         self.assertAlmostEqual(value,katta(2,3,4),4)
+# unittest.main()
+
+
+# from m36_Task import fibonachi
+
+# class fib(unittest.TestCase):
+#     def fibnt(self):
+#         self.assertTrue(fibonachi(3))
+#         self.assertTrue(fibonachi(5))
+#         self.assertTrue(fibonachi(8))
+#         self.assertTrue(fibonachi(13))
+#         self.assertTrue(fibonachi(21))
+      
+#     def fibnf(self):
+#         self.assertFalse(fibonachi(3))
+#         self.assertFalse(fibonachi(7))
+#         self.assertFalse(fibonachi(9))
+#         self.assertFalse(fibonachi(12))
+#         self.assertFalse(fibonachi(19))
+# unittest.main()
