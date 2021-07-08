0d27556a680611a81cf32048fb44b880009f9805
Shukurali <81503330+ShukurDev@users.noreply.github.com>
2021-07-08 04:51:08 -0700

    complete

---
+++ #2_Task.py
@@ -0,0 +1,19 @@
+# 5 ning 4-darajasini toping
+print(5**4)
+
+#22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print(22%4)
+
+#Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+a=125
+print("Yuzi:",a*a,"\nPeremetri:",4*a)
+
+import math
+# Diametri 12 ga teng bo'lgan doiraning yuzini toping  ( deb oling)
+d=12
+print("Doiraning Yuzi:",math.pi*d*d/4)
+
+#Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping 
+a=6
+b=7
+print("Gepatenuza:",math.sqrt(a*a+b*b))
---
+++ #3_Task.py
@@ -0,0 +1,19 @@
+# "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+a = "Hello World!"
+print(a)
+
+# xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring,
+#  keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+xabar = "Shuningdek o'zgaruvchilarga Pythonda ishlatiladigan funktsiyalar va maxsus kalit so'zlarning (keywords) nomini bermang. Kalit so'zlar ro'yhatini ko'rish uchun Spyder konsolida avval help() deb yozing va Enter tugmasini bosing. Keyin esa keywords deb kiritib, yana Enter bosing. Marhamat, ekraningizda Pythondagi maxsus kalit so'zlar ro'yhatini ko'ryapsiz:"
+print(xabar)
+xabar = "Qalesan Shukurali"
+print(xabar)
+
+# class den nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
+# u bunaqangi 'class' nomli uzgaruchi yaratib bulmaydi chunki u keyword so'z
+
+# Quyidagi kodni bajaring:
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
